ea99c1759b118c14fe79051f5741dcbd35d22b5a
Tanuló 301-04 <301-04@KKSZKI.LOCAL>
2021-11-16 18:47:23 +0100

    Kész az összes feladat.

--- proba.py
+++ proba.py
@@ -1,3 +1,34 @@
 a=int(input("Kérek egy számot!"))
 b=int(input("Kérek egy másik számot!"))
 print(a+b)
+
+
+import random
+list=[]
+for i in range(10):
+    list.append(random.randrange(1,30))
+
+szamok=len(list)
+összeg=sum(list)
+atlag=összeg/szamok
+print(atlag)
+
+
+minimum=min(list)
+maximum=max(list)
+print(minimum)
+print(maximum)
+
+#eldöntés tétele
+vane=False
+for i in list:
+    if i==9:
+        vane=True
+
+if vane==True:
+    print("Van  benne 9-es!")
+
+else:
+    print("Nincs benne 9-es!")
+
+print(list)
