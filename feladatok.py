8a7b36d37696c451e73017d86a830cef342468b8
Tanuló 301-04 <301-04@KKSZKI.LOCAL>
2021-11-30 18:48:59 +0100

    4 feladat kész

--- feladatok.py
+++ feladatok.py
@@ -1,1 +1,32 @@
-print("Hello")
+#Első feladat
+
+#for i in range(1,11):
+    #print(i,"---",1/i)
+
+#Második feladat
+
+#a=int(input("Kérem a hatványalapot: "))
+#b=int(input("Kérem a hatványkitevőt: "))
+
+#hatvanyertek=a**b
+
+#print(hatvanyertek)
+
+#Harmadik feladat
+
+while True:
+    c=int(input("Kérem egy pozitív számot: "))
+    if c<=0:
+        print(c)
+    else:
+        break
+
+#Negyedik feladat
+
+a=int(input("Kérem az első számot: "))
+b=int(input("Kérem a második számot: "))
+
+print(-(a-b))
+
+#Ötödik feladat
+
